f022e100c1bdc2acac0dc0c2b14ea119b0b33bc4
daby6 <daby6789@gmail.com>
2022-11-09 19:32:57 +0800

    Add configParser

--- fastapi/app/__init__.py
+++ fastapi/app/__init__.py
@@ -1,35 +1,26 @@
+import configparser
 from fastapi import FastAPI
 from .routers import example
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+config = configparser.ConfigParser()
+config.read('app.ini')
+appConfig = config['app']
+uvicornConfig = config['uvicorn']
+
+
 app = FastAPI()
 app.include_router(example.router)
-
-origins = [
-    "http://localhost:8000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["Content-Type"],
+    **appConfig
 )
 
-config = uvicorn.Config(
-    app="app:app",
-    host="0.0.0.0",
+asgiConfig = uvicorn.Config(
     port=8080,
-    workers=4,
-    reload=True,
-    # thread = 2,
-    # master = true,
-    # chmod-socket = 660
-    # vacuum = true
-    # die-on-term = true
+    **uvicornConfig,
 )
 
-server = uvicorn.Server(config)
+server = uvicorn.Server(asgiConfig)
 
