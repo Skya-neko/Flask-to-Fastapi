05e6e4892e6471381c8f57c47d763d7b104db391
daby6 <daby6789@gmail.com>
2022-11-09 23:08:48 +0800

    connect to psql and return to front end

--- fastapi/app/routers/example.py
+++ fastapi/app/routers/example.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
-
+from ..models.customer import *
 router = APIRouter()
 
 
 @router.get('/endpoint1')
 def getAdsData():
     return 'welcome to test'
+
+@router.get('/customer/{id}')
+def getPersonalData():
+    page = customer(con)
+    jsonData = page.select_info()
+    return jsonData
+
